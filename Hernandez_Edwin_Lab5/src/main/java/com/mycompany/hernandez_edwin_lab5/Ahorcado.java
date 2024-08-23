2dbcae775b93c6777d006dce32a234c79d3a38db
EdwinHn <162158782+EdwinHn@users.noreply.github.com>
2024-08-23 11:52:21 -0600

    LAB #5
    el main se llama "Ahorcado"

---
+++ Hernandez_Edwin_Lab5/src/main/java/com/mycompany/hernandez_edwin_lab5/Ahorcado.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hernandez_edwin_lab5;
+
+import static java.awt.SystemColor.menu;
+import javax.swing.JFrame;
+import java.util.Scanner;
+
+/**
+ *
+ * @author edwinhernandezlopez
+ */
+public class Ahorcado {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner (System.in);
+        
+        new Menu().setVisible(true);
+        
+        
+    }
+    
+}
